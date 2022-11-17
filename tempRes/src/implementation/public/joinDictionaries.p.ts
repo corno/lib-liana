b8c001a87d4aded181507a6e802aad641c42be53
corno <corno.astn.a@schraverus.com>
2022-11-17 22:58:42 +0100

    .

--- tempRes/src/implementation/public/joinDictionaries.p.ts
+++ tempRes/src/implementation/public/joinDictionaries.p.ts
@@ -5,15 +5,10 @@ import * as api from "../../interface"
 
 export const joinDictionaries: api.FJoinDictionaries = ($) => {
     function imp<T>($: pt.Dictionary<pt.Dictionary<T>>): pt.Dictionary<T> {
-        const x = ps.createDictionaryBuilder<T>(
-            ["ignore", {}],
-            ($) => {
-                throw new Error("KEY CLASH")
-            }
-        )
+        const x = ps.createUnsafeDictionaryBuilder<T>()
         $.map(($, key) => {
             $.map(($, key2) => {
-                x.add(key + key2, $)
+                x.add(`${key}${key2}`, $)
             })
         })
         return x.getDictionary()
