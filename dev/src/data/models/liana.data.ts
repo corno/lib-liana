94f71f1b6c287cae720b6cddca18a3791999b5bf
corno <corno.astn.a@schraverus.com>
2023-02-27 08:29:59 +0100

    .

--- dev/src/data/models/liana.data.ts
+++ dev/src/data/models/liana.data.ts
@@ -24,6 +24,71 @@ export const $: gliana.T.Model<pd.SourceLocation> = {
     'global types': d({
         "Glossary": globalType({}, group({
         })),
+
+        // "GlobalType": type(group({
+        //     "parameters": member(parametrizedReference("common", {}, "AnnotatedDictionary", { "Annotation": typeReference("Annotation"), "Type": typeReference("Parameter") })),
+        //     "type": member(reference("LocalType")),
+        // })),
+        // "LocalType": type(taggedUnion({
+        //     "string": reference("String"),
+        //     "boolean": group({}),
+        //     "dictionary": group({
+        //         "key": member(reference("String")),
+        //         "type": member(reference("LocalType")),
+        //     }),
+        //     "array": group({
+        //         "type": member(reference("LocalType")),
+        //     }),
+        //     "optional": group({
+        //         "type": member(reference("LocalType")),
+        //     }),
+        //     "tagged union": group({
+        //         "options": member(parametrizedReference("common", {}, "AnnotatedDictionary", {
+        //             "Annotation": typeReference("Annotation"),
+        //             "Type": typeReference("LocalType"),
+        //         })),
+        //         "default": member(parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey")),
+        //     }),
+        //     "group": group({
+        //         "properties": member(reference("Properties")),
+        //     }),
+        //     "component": group({
+        //         "type": member(parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey")),
+        //         "arguments": member(parametrizedReference("common", {}, "AnnotatedDictionary", { "Annotation": typeReference("Annotation"), "Type": typeReference("common", "Null") })),
+        //     }),
+        // })),
+        // "Model": type(group({
+        //     "string types": member(parametrizedReference("common", {}, "AnnotatedDictionary", { "Annotation": typeReference("Annotation"), "Type": typeReference("common", "Null") })),
+        //     "global types": member(parametrizedReference("common", {}, "AnnotatedDictionary", { "Annotation": typeReference("Annotation"), "Type": typeReference("GlobalType") })),
+        //     "root": member(parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey")),
+        // })),
+        // "Parameter": type(parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey")),
+        // "Property": type(group({
+        //     "sibling dependencies": member(parametrizedReference("common", {}, "AnnotatedDictionary", { "Annotation": typeReference("Annotation"), "Type": typeReference("common", "Null") })),
+        //     "type": member(reference("LocalType")),
+        // })),
+        // "Properties": type(parametrizedReference("common", {}, "AnnotatedDictionary", { "Annotation": typeReference("Annotation"), "Type": typeReference("Property") })),
+        // "Reference": type(group({
+        //     "type": member(taggedUnion({
+        //         "parameter": parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey"),
+        //         "sibling": parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey"),
+        //         "other": group({}),
+        //     })),
+        //     "steps": member(array(taggedUnion({
+        //         "group": parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey"),
+        //         "tagged union": parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey"),
+        //         "reference": group({}),
+        //         "array": group({}),
+        //     }))),
+        // })),
+        // "String": type(group({
+        //     "constrained": member(taggedUnion({
+        //         "no": group({
+        //             "type": member(parametrizedReference("common", { "Annotation": typeReference("Annotation") }, "AnnotatedKey")),
+        //         }),
+        //         "yes": reference("Reference"),
+        //     })),
+        // })),
     }),
     'root': r("Glossary"),
 }
