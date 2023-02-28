ca69fe9260ec95f27255804bb8581664ef18c639
corno <corno.astn.a@schraverus.com>
2023-02-28 20:05:58 +0100

    .

--- dev/src/data/data.data.ts
+++ dev/src/data/data.data.ts
@@ -1,34 +1,37 @@
 import * as pd from 'pareto-core-data'
 import * as pt from 'pareto-core-types'
+import * as pv from 'pareto-core-dev'
 
 import * as gliana2pareto from "lib-liana/dist/submodules/liana2pareto"
 
 import { $ as liana } from "./models/liana.data"
 
-export const $: pt.Array<gliana2pareto.T.GenerateModuleData<pd.SourceLocation>> = pd.a([
-    {
-        'path': `../../pareto/src/data/submodules/liana`,
-        'data': {
-            'configuration': {
-                'datamodel': [true, {
-                    'annotations': true,
-                    'properties optional': false,
-                    'reference mapping': ['string', {}],
-                }],
-                'visitor interface': [true, {
-                    'datamodel location': "FIXME"
-                }],
-                'algorithms': {
-                    'serialize': [false],
-                },
-            },
-            'mappedModel': {
-                'model': liana,
+pv.logDebugMessage("REENABLE MODEL")
 
-                'stringmapping': pd.d({
-                    "identifier": ['string', null]
-                }),
-            },
-        }
-    }
+export const $: pt.Array<gliana2pareto.T.GenerateModuleData<pd.SourceLocation>> = pd.a([
+    // {
+    //     'path': `../../pareto/src/data/submodules/liana`,
+    //     'data': {
+    //         'configuration': {
+    //             'datamodel': [true, {
+    //                 'annotations': true,
+    //                 'properties optional': false,
+    //                 'reference mapping': ['string', {}],
+    //             }],
+    //             'visitor interface': [true, {
+    //                 'datamodel location': "FIXME"
+    //             }],
+    //             'algorithms': {
+    //                 'serialize': [false],
+    //             },
+    //         },
+    //         'mappedModel': {
+    //             'model': liana,
+
+    //             'stringmapping': pd.d({
+    //                 "identifier": ['string', null]
+    //             }),
+    //         },
+    //     }
+    // }
 ])
