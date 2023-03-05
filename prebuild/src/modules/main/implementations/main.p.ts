72189d41baaa8ec5d67dee27b1e6b721b926514c
corno <corno.astn.a@schraverus.com>
2023-03-05 18:03:48 +0100

    .

--- prebuild/src/modules/main/implementations/main.p.ts
+++ prebuild/src/modules/main/implementations/main.p.ts
@@ -1,4 +1,8 @@
+import * as pv from 'pareto-core-dev'
+import * as pd from 'pareto-core-data'
+
 import * as gliana2pareto from "lib-liana/dist/submodules/liana2glossary"
+import * as gresolve from "lib-liana/dist/submodules/resolve"
 
 import { $ as data } from "../../../data/data.data"
 
@@ -7,5 +11,9 @@ import { main } from "../api"
 export const $$: main = ($) => {
     data.__forEach(($) => {
         gliana2pareto.$a.generate($)
+        gresolve.$a.createResolver({})($.data['mapped model'].model, ($) => {
+            pv.logDebugMessage(`${$.message} @ ${$.annotation.file}:${$.annotation.line}:${$.annotation.column}`)
+        })
+
     })
 }
