867c32da3d577c91adb394b51c4f5bb1623d6e7c
corno <corno.astn.a@schraverus.com>
2023-03-09 21:43:48 +0100

    .

--- typescript/pub/src/main/pure/implementations/compile.p.ts
+++ typescript/pub/src/main/pure/implementations/compile.p.ts
@@ -1,4 +1,5 @@
 import * as gresolve from "../../../submodules/resolve"
+import * as gl2g from "../../../submodules/liana2glossary"
 
 import { compile } from "../api.generated"
 
@@ -6,5 +7,6 @@ export const $$: compile = ($, $i) => {
 
     $.outputs.__forEach(($) => {
         gresolve.$a.createResolver({})($.data['mapped model'].model, $i)
+        gl2g.$b.generate($)
     })
 }
