bb550cb8bdb768eaf542c3112a8b4ebdc0296b4d
corno <corno.astn.a@schraverus.com>
2023-03-27 13:44:17 +0200

    .

--- typescript/pub/src/submodules/liana2algorithm/implementation.generated.ts
+++
@@ -1,8 +0,0 @@
-import { API } from "./api.generated"
-import { $$ as imapLiana2Serializer } from "./implementations/mapLiana2Serializer.s.f"
-import { $$ as imapLiana2States } from "./implementations/mapLiana2States.s.f"
-
-export const $api: API = {
-    'mapLiana2Serializer': imapLiana2Serializer,
-    'mapLiana2States': imapLiana2States,
-}
--- typescript/pub/src/submodules/resolve/glossary/datatypes.generated.ts
+++
@@ -1,7 +0,0 @@
-import * as pt from 'pareto-core-types'
-
-import * as g_common from "glo-pareto-common"
-import * as g_liana from "../../liana"
-import * as g_liana_resolved from "../../liana_resolved"
-
-export namespace T {}
