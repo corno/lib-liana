e916b772ffd7fed21ad2de327fd583051ca2d817
corno <corno.astn.a@schraverus.com>
2023-04-26 11:37:10 +0200

    .

--- typescript/pub/src/submodules/liana/shorthands.ts
+++ typescript/pub/src/submodules/liana/shorthands.ts
@@ -136,6 +136,18 @@ export function pNonCyclicSiblings(
     }
 }
 
+export function snothing(
+    tail?: g_this.T.Optional__Value__Selection__Tail<pd.SourceLocation>
+): g_this.T.Optional__Value__Selection__Tail<pd.SourceLocation> {
+    return [true, {
+        'step type': ['nothing', {
+            'annotation': pd.getLocationInfo(1),
+            'content': null,
+        }],
+        'tail': tail === undefined ? [false] : tail
+    }]
+}
+
 export function scomponent(
     tail?: g_this.T.Optional__Value__Selection__Tail<pd.SourceLocation>
 ): g_this.T.Optional__Value__Selection__Tail<pd.SourceLocation> {
