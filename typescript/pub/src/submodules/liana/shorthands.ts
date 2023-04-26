36f8c7b5510a571cd2e56e04424ead7f0ea1073c
corno <corno.astn.a@schraverus.com>
2023-04-26 12:05:30 +0200

    .

--- typescript/pub/src/submodules/liana/shorthands.ts
+++ typescript/pub/src/submodules/liana/shorthands.ts
@@ -160,6 +160,19 @@ export function soptional(
     }]
 }
 
+export function snothing2(
+    tail?: g_this.T.Optional__Value__Selection__Tail<pd.SourceLocation>
+): g_this.T.Optional__Value__Selection__Tail<pd.SourceLocation> {
+    return [true, {
+        'step type': ['nothing', {
+            'annotation': pd.getLocationInfo(1),
+            'content': null,
+        }],
+        'tail': tail === undefined ? [false] : tail
+    }]
+}
+
+
 export function snothing(
     tail?: g_this.T.Optional__Value__Selection__Tail<pd.SourceLocation>
 ): g_this.T.Optional__Value__Selection__Tail<pd.SourceLocation> {
